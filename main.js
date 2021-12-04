d346ccce6e34d92ae2b0c407bf6d82aeb05a3544
Lanchenbiii <87656915+Lanchenbiii@users.noreply.github.com>
2021-12-04 19:57:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -10,4 +10,26 @@ recognition.onresult = function run (event){
     var Content = event.results[0][0].transcript ;
     console.log(Content);
     document.getElementById("textbox").innerHTML = Content;
+    if(Content == "take my selfie"){
+        console.log("taking selfie");
+        speak();
+    }
 }
+function speak(){
+var synth = window.speechSynthesis;
+speak_data = "Taking your Selfie in 5 seconds";
+var utterThis = new SpeechSynthesisUtterance(speak_data);
+synth.speak(utterThis);
+Webcam.attach(camera);
+setTimeout(function(){
+    take_snapshot();
+}, 5000);
+}
+camera = document.getElementById("camera");
+Webcam.set({
+width: 320,
+height: 240,
+image_format: 'jpeg',
+jpeg_quality: 90
+});
+
